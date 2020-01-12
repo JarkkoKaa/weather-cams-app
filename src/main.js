8bdd553341aa3a22f8e2a414e994cdc3b0fb7dae
jarkkokaa <jarkkomkaartinen@gmail.com>
2020-01-12 21:39:32 +0200

    init commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+// import bootstrap Vue css files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// import bootstrap Vue
+import { BootstrapVue } from "bootstrap-vue";
 Vue.config.productionTip = false;
 
 new Vue({
+  BootstrapVue,
   render: h => h(App)
 }).$mount("#app");
